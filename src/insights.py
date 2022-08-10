68d7ff93e18e3eeec23ff011466f02f0354502a7
mabiiak <mabiane.polniak@gmail.com>
2022-08-10 10:55:59 -0300

    feat: req 3

--- src/insights.py
+++ src/insights.py
@@ -29,21 +29,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    industries_data = read(path)
+    industries_types = set()
+    for industry in industries_data:
+        if industry["industry"] != "":
+            industries_types.add(industry["industry"])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return industries_types
 
 
 def filter_by_industry(jobs, industry):
