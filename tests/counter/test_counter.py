595dabb9270a65612005a2674d09baccc3ce736d
mabiiak <mabiane.polniak@gmail.com>
2022-08-11 23:25:05 -0300

    test: req 10 - testa funçao counter word

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    word_javascript = count_ocurrences('src/jobs.csv', 'javascript')
+    assert word_javascript == 122
+
+    word_python = count_ocurrences('src/jobs.csv', 'python')
+    assert word_python == 1639
