6b28854503a005c3c30ef3decdc4aef7d26b35fe
mabiiak <mabiane.polniak@gmail.com>
2022-08-12 10:36:38 -0300

    test: req 11 - testa funçao test brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    dict_translate = {
+        "title": "Maquinista", "salary": "2000", "type": "trainee"
+        }
+    function_test = read_brazilian_file("tests/mocks/brazilians_jobs.csv")[0]
+
+    assert function_test == dict_translate
